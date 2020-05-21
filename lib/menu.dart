7c54e5a411d11481a66acbe941a8f7685050dc87
Taki Uddin <takiuddin93@gmail.com>
2020-05-21 22:52:58 +0600

    slight ui changes to menu

--- lib/menu.dart
+++ lib/menu.dart
@@ -4,121 +4,88 @@ import 'package:flutter/material.dart';
 import 'package:aakcupchaa/universal_variables.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+bool hasPosts;
+
 class Menu extends StatefulWidget {
   @override
   _MenuState createState() => _MenuState();
 }
 
 class _MenuState extends State<Menu> {
-  bool hasPosts = false;
   @override
   Widget build(BuildContext context) {
     Size media = MediaQuery.of(context).size;
     double width = media.width;
     double height = media.height;
     return Center(
-      child: Column(
+      child: Stack(
         children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 0.0,
-                top: height * 0.04,
-                right: 0.0,
-                bottom: 0.0,
+          Positioned(
+            child: Container(
+              width: width,
+              height: height * 0.08,
+              padding: EdgeInsets.symmetric(
+                vertical: height * 0.02,
+                horizontal: 0.0,
               ),
-              child: RichText(
-                text: TextSpan(children: <TextSpan>[
-                  TextSpan(
-                    text: "Recent posts",
-                    style: TextStyle(
-                        color: UniversalVariables.primaryEbony,
-                        fontSize: 18.0,
-                        fontFamily: 'FuturaPTBook'),
+              decoration: BoxDecoration(
+                  color: UniversalVariables.primaryCrimson,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(0.0),
+                    topRight: Radius.circular(0.0),
+                    bottomRight: Radius.circular(4.0),
+                    bottomLeft: Radius.circular(0.0),
                   ),
-                ]),
+                  boxShadow: [
+                    BoxShadow(
+                        color: UniversalVariables.primaryEbony,
+                        offset: Offset(0.0, 1.0),
+                        blurRadius: 4.0)
+                  ]),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 8,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(children: <TextSpan>[
+                          TextSpan(
+                            text: "Recent Feeds",
+                            style: TextStyle(
+                                color: UniversalVariables.primaryAlabaster,
+                                fontSize: 18.0,
+                                fontFamily: 'FuturaPTBook'),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: SvgPicture.asset(
+                        'assets/svgs/Notifications.svg',
+                        color: UniversalVariables.primaryAlabaster,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          Expanded(
-            flex: 9,
-            child: Padding(
-              padding: EdgeInsets.only(
-                left: 0.0,
-                top: height * 0.0,
-                right: 0.0,
-                bottom: 0.0,
+          Positioned(
+            child: Container(
+              margin: EdgeInsets.only(
+                top: height * 0.08,
               ),
-              child: hasPosts
-                  ? Posts()
-                  : Column(
-                      children: <Widget>[
-                        SvgPicture.asset('assets/svgs/BarrenClipBoard.svg'),
-                        RichText(
-                          text: TextSpan(children: <TextSpan>[
-                            TextSpan(
-                              text: "Why so barren?",
-                              style: TextStyle(
-                                  color: UniversalVariables.primaryCrimson,
-                                  fontSize: 18.0,
-                                  fontFamily: 'FuturaPTMedium'),
-                            ),
-                          ]),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            left: 0.0,
-                            top: height * 0.08,
-                            right: 0.0,
-                            bottom: 0.0,
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: UniversalVariables.primaryCrimson,
-                                borderRadius: BorderRadius.circular(2.0)),
-                            child: FlatButton(
-                                onPressed: () {
-                                  setState(() {
-                                    hasPosts
-                                        ? hasPosts = false
-                                        : hasPosts = true;
-                                  });
-                                  print("Explore creators");
-                                },
-                                child: RichText(
-                                  text: TextSpan(children: <TextSpan>[
-                                    TextSpan(
-                                      text: "Explore creators",
-                                      style: TextStyle(
-                                          color: UniversalVariables
-                                              .primaryAlabaster,
-                                          fontSize: 18.0,
-                                          fontFamily: 'FuturaPTHeavy'),
-                                    ),
-                                  ]),
-                                )),
-                          ),
-                        )
-                      ],
-                    ),
+              child: Posts(),
             ),
           ),
         ],
       ),
     );
-  }
-}
-
-class Posts extends StatefulWidget {
-  @override
-  _PostsState createState() => _PostsState();
-}
-
-class _PostsState extends State<Posts> {
-  @override
-  Widget build(BuildContext context) {
-    return CustomPosts();
   }
 }
 
@@ -130,26 +97,102 @@ class _PostsState extends State<Posts> {
 // class _PostsState extends State<Posts> {
 //   @override
 //   Widget build(BuildContext context) {
-//     return Container(
-//       child: FutureBuilder(
-//         builder: (context, snapshot) {
-//           if (snapshot.hasData) {
-//             var showData = json.decode(snapshot.data.toString());
-//             return ListView.builder(
-//               itemCount: showData.length,
-//               itemBuilder: (BuildContext context, int index) {
-//                 return ListTile(
-//                   title: Text(showData['posts'][index]['pTitle']),
-//                   subtitle: Text(showData['posts'][index]['pContent']),
-//                 );
-//               },
-//             );
-//           }
-//           return Center(child: CircularProgressIndicator());
-//         },
-//         future:
-//             DefaultAssetBundle.of(context).loadString("assets/json/posts.json"),
-//       ),
-//     );
+//     return CustomPosts();
 //   }
 // }
+
+class Posts extends StatefulWidget {
+  @override
+  _PostsState createState() => _PostsState();
+}
+
+class _PostsState extends State<Posts> {
+  @override
+  Widget build(BuildContext context) {
+    Size media = MediaQuery.of(context).size;
+    double width = media.width;
+    double height = media.height;
+    return Container(
+      child: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var showData = json.decode(snapshot.data.toString());
+            if (showData['noofposts'] != "0") {
+              return ListView.builder(
+                itemCount: showData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return CustomPosts(
+                    title: showData['posts'][index]['pTitle'],
+                    subtitle: showData['posts'][index]['pContent'],
+                    author: showData['posts'][index]['pAuthor'],
+                  );
+                },
+              );
+            } else {
+              return Column(
+                children: <Widget>[
+                  Container(
+                      width: width,
+                      margin: EdgeInsets.only(top: height * 0.04),
+                      child:
+                          SvgPicture.asset('assets/svgs/BarrenClipBoard.svg')),
+                  Container(
+                    width: width,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: RichText(
+                        text: TextSpan(children: <TextSpan>[
+                          TextSpan(
+                            text: "Why so barren?",
+                            style: TextStyle(
+                                color: UniversalVariables.primaryCrimson,
+                                fontSize: 18.0,
+                                fontFamily: 'FuturaPTMedium'),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: width,
+                    padding: EdgeInsets.only(
+                      left: width * 0.16,
+                      top: height * 0.08,
+                      right: width * 0.16,
+                      bottom: 0.0,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: UniversalVariables.primaryCrimson,
+                          borderRadius: BorderRadius.circular(2.0)),
+                      child: FlatButton(
+                          onPressed: () {
+                            Posts();
+                            print("Explore creators");
+                          },
+                          child: RichText(
+                            text: TextSpan(children: <TextSpan>[
+                              TextSpan(
+                                text: "Explore creators",
+                                style: TextStyle(
+                                    color: UniversalVariables.primaryAlabaster,
+                                    fontSize: 18.0,
+                                    fontFamily: 'FuturaPTHeavy'),
+                              ),
+                            ]),
+                          )),
+                    ),
+                  )
+                ],
+              );
+            }
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+        future:
+            DefaultAssetBundle.of(context).loadString("assets/json/posts.json"),
+      ),
+    );
+  }
+}

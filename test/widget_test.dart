48baf63d59c8b60b86bd4d5cdc6393aea226d6e9
Taki Uddin <takiuddin93@gmail.com>
2020-05-22 17:27:38 +0600

    playing with test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,44 +5,36 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:aakcupchaa/universal_variables.dart';
 import 'package:flutter/material.dart';
+import 'package:aakcupchaa/menu.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:aakcupchaa/dashboard.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Find Menu Buttons', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(DashBoard());
+    await tester.pumpWidget(Menu());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Explore creators'), findsOneWidget);
+    expect(find.text(''), findsNothing);
 
     // Tap 'Menu' and trigger a frame.
-    await tester.tap(find.byWidget(SvgPicture.asset('assets/svgs/Menu.svg')));
+    await tester.tap(find.byWidget(RichText(
+        text: TextSpan(children: <TextSpan>[
+      TextSpan(
+        text: "Explore creators",
+        style: TextStyle(
+            color: UniversalVariables.primaryAlabaster,
+            fontSize: 18.0,
+            fontFamily: 'FuturaPTHeavy'),
+      ),
+    ]))));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-
-    // Tap 'Stats' and trigger a frame.
-    await tester.tap(find.byWidget(SvgPicture.asset('assets/svgs/Stats.svg')));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-
-    // Tap 'Settings' and trigger a frame.
-    await tester
-        .tap(find.byWidget(SvgPicture.asset('assets/svgs/Settings.svg')));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byWidget(Menu()), findsOneWidget);
+    expect(find.byWidget(null), findsNothing);
   });
 }

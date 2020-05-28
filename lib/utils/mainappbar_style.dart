a912cf14c357d155c5228cc4076bd1cc04ece302
Taki Uddin <takiuddin93@gmail.com>
2020-05-28 22:41:47 +0600

    added some utility files for mainappbar

---
+++ lib/utils/mainappbar_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:aakcupchaa/utils/widget_utils.dart';
+
+double appBarHeight(BuildContext context) {
+  return screenAwareSize(48.0, context);
+}

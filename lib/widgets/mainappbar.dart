5c46701886ad236e9ccf095e3abe485c2ab7f9cd
Taki Uddin <takiuddin93@gmail.com>
2020-05-28 22:42:22 +0600

    created mainappbar.dart file

---
+++ lib/widgets/mainappbar.dart
@@ -0,0 +1,149 @@
+import 'package:aakcupchaa/dashboard.dart';
+import 'package:aakcupchaa/menu.dart';
+import 'package:aakcupchaa/universal_variables.dart';
+import 'package:aakcupchaa/utils/mainappbar_style.dart';
+import 'package:flutter/material.dart';
+
+class MainAppBar extends StatelessWidget {
+  final String title, back;
+
+  const MainAppBar({Key key, this.title, this.back}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        height: appBarHeight(context),
+        color: UniversalVariables.primaryCrimson,
+        child: Padding(
+          padding: EdgeInsets.all(0.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: _buildleading(context),
+                ),
+              ),
+              Expanded(
+                flex: 6,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: _buildLabel(context),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: _buildaction(context),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Builder _buildleading(BuildContext context) {
+    IconData _iconData;
+    switch (back.toString()) {
+      case "menu":
+        {
+          _iconData = null;
+        }
+        break;
+      case "search":
+        {
+          _iconData = Icons.arrow_back;
+        }
+        break;
+      default:
+        {
+          Navigator.pop(context);
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => DashBoard()));
+        }
+        break;
+    }
+    return Builder(
+      builder: (context) => IconButton(
+        color: UniversalVariables.primaryAlabaster,
+        icon: new Icon(
+          _iconData,
+          color: UniversalVariables.primaryAlabaster,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => DashBoard()));
+        },
+      ),
+    );
+  }
+
+  Align _buildLabel(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: RichText(
+        text: TextSpan(
+          style: new TextStyle(
+            fontFamily: 'FuturaPTBool',
+            fontSize: 16.0,
+            color: UniversalVariables.primaryAlabaster,
+          ),
+          children: [
+            TextSpan(
+              text: title,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Builder _buildaction(BuildContext context) {
+    String _alertdialogTitle, _alertdialogDescription, _alertdialogButton;
+    if (title == 'Recent Feeds') {
+      _alertdialogTitle = 'Work in Progress';
+      _alertdialogDescription = 'This feature has not been implemented yet!';
+      _alertdialogButton = 'Back';
+    } else {}
+    return Builder(
+      builder: (context) => IconButton(
+        color: UniversalVariables.primaryAlabaster,
+        icon: new Icon(Icons.notifications),
+        onPressed: () {
+          showDialog<void>(
+            context: context,
+            barrierDismissible: false, // user must tap button!
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(_alertdialogTitle),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[
+                      Text(_alertdialogDescription),
+                    ],
+                  ),
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text(_alertdialogButton),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}

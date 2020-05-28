2fdaaeceb6e12a33792a272739870bd001f2eee8
Taki Uddin <takiuddin93@gmail.com>
2020-05-28 23:17:26 +0600

    menu.dart slightly modified

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:aakcupchaa/search.dart';
 import 'package:aakcupchaa/widgets/custom_notification_dialog_widget.dart';
 import 'package:aakcupchaa/widgets/custom_post_widget.dart';
+import 'package:aakcupchaa/widgets/mainappbar.dart';
 import 'package:flutter/material.dart';
 import 'package:aakcupchaa/universal_variables.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -24,102 +26,30 @@ class _MenuState extends State<Menu> {
           Positioned(
             child: Container(
               width: width,
-              height: height * 0.08,
+              height: height * 0.10,
               padding: EdgeInsets.symmetric(
-                vertical: height * 0.02,
+                vertical: height * 0.0,
                 horizontal: 0.0,
               ),
               decoration: BoxDecoration(
                   color: UniversalVariables.primaryCrimson,
                   shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(0.0),
-                    topRight: Radius.circular(0.0),
-                    bottomRight: Radius.circular(4.0),
-                    bottomLeft: Radius.circular(0.0),
-                  ),
                   boxShadow: [
                     BoxShadow(
                         color: UniversalVariables.primaryEbony,
                         offset: Offset(0.0, 1.0),
                         blurRadius: 4.0)
                   ]),
-              child: Stack(
-                children: [
-                  Positioned(
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: RichText(
-                        text: TextSpan(children: <TextSpan>[
-                          TextSpan(
-                            text: "Recent Feeds",
-                            style: TextStyle(
-                                color: UniversalVariables.primaryAlabaster,
-                                fontSize: 16.0,
-                                fontFamily: 'FuturaPTBool'),
-                          ),
-                        ]),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: GestureDetector(
-                        onTap: () {
-                          CustomDialog.showScaleAlertBox(
-                            context: context,
-                            title: "Alert",
-                            icon: Icons.info_outline, // IF YOU WANT TO ADD ICON
-                            text:
-                                'Feature not yet implemented!', // IF YOU WANT TO ADD
-                            firstButton: "Ok",
-                          );
-                          print("Notification Clicked");
-                        },
-                        child: Stack(
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              SvgPicture.asset(
-                                'assets/svgs/Notifications.svg',
-                                color: UniversalVariables.primaryAlabaster,
-                              ),
-                              Container(
-                                width: width * 0.032,
-                                height: width * 0.032,
-                                decoration: BoxDecoration(
-                                  color: UniversalVariables.primaryCrimson,
-                                  shape: BoxShape.rectangle,
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: RichText(
-                                    text: TextSpan(children: <TextSpan>[
-                                      TextSpan(
-                                        text: "9+",
-                                        style: TextStyle(
-                                            color: UniversalVariables
-                                                .primaryAlabaster,
-                                            fontSize: 8.0,
-                                            fontFamily: 'FuturaPTBook'),
-                                      ),
-                                    ]),
-                                  ),
-                                ),
-                              ),
-                            ]),
-                      ),
-                    ),
-                  ),
-                ],
+              child: PreferredSize(
+                child: MainAppBar(back: "menu", title: "Recent Feeds"),
+                preferredSize: Size.fromHeight(media.height),
               ),
             ),
           ),
           Positioned(
             child: Container(
               margin: EdgeInsets.only(
-                top: height * 0.08,
+                top: height * 0.10,
               ),
               child: FeedPosts(),
             ),
@@ -197,7 +127,11 @@ class _FeedPostsState extends State<FeedPosts> {
                           borderRadius: BorderRadius.circular(2.0)),
                       child: FlatButton(
                           onPressed: () {
-                            FeedPosts();
+                            Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Search()),
+                                (Route<dynamic> route) => false);
                             print("Explore creators");
                           },
                           child: RichText(

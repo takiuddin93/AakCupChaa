56b89e9b4808589e9426ea50188609fe516ec2e3
Taki Uddin <takiuddin93@gmail.com>
2020-06-14 02:19:37 +0600

    new custom notification dialog and post widgets added

--- lib/widgets/custom_notification_dialog_widget.dart
+++ lib/widgets/custom_notification_dialog_widget.dart
@@ -1,4 +1,4 @@
-import 'package:aakcupchaa/universal_variables.dart';
+import 'package:aakcupchaa/utils/universal_variables.dart';
 import 'package:flutter/material.dart';
 
 class CustomDialog {
--- lib/widgets/custom_post_widget.dart
+++ lib/widgets/custom_post_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:aakcupchaa/universal_variables.dart';
+import 'package:aakcupchaa/utils/universal_variables.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomPosts extends StatelessWidget {

13000a87d0c77ac24e8d5df673d68e518dd6bb34
Taki Uddin <takiuddin93@gmail.com>
2020-06-14 02:17:43 +0600

    new universal variables modified

---
+++ lib/utils/universal_variables.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UniversalVariables {
+  static final Color transparent = Color(0x00FFFFFF);
+  static final Color primaryCrimson = Color(0xFFDA2323);
+  static final Color primaryCrimsonShadow = Color(0x29DA2323);
+  static final Color primaryAlabaster = Color(0xFFFAFAFA);
+  static final Color primaryRiverBed = Color(0xFF424C55);
+  static final Color primaryEbony = Color(0xFF04080F);
+  static final Color primaryEbonyShadow = Color(0x2904080F);
+  static final Color primaryGhost = Color(0xFFCACFD6);
+  static final Color primaryGhostShadow = Color(0x29CACFD6);
+}

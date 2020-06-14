dae8aa754d7cd1db11956077b7fad0ccd754b6b9
Taki Uddin <takiuddin93@gmail.com>
2020-06-14 20:31:20 +0600

    search screen modified to keep maunappbar height same

--- lib/search.dart
+++ lib/search.dart
@@ -1,51 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:aakcupchaa/widgets/mainappbar.dart';
+import 'package:aakcupchaa/utils/universal_variables.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import 'widgets/mainappbar.dart';
-
-class Search extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class Search extends StatefulWidget {
+  Search({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MySearchState createState() => _MySearchState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MySearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     Size media = MediaQuery.of(context).size;
-    return Stack(children: <Widget>[
-      Scaffold(
-          backgroundColor: Colors.white,
-          appBar: PreferredSize(
-            child: MainAppBar(back: "search", title: "Search"),
-            preferredSize: Size.fromHeight(media.height),
+    double width = media.width;
+    double height = media.height;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: media.height * 0.0,
+            child: Container(
+              width: width,
+              height: height * 0.10,
+              padding: EdgeInsets.symmetric(
+                vertical: height * 0.0,
+                horizontal: 0.0,
+              ),
+              decoration: BoxDecoration(
+                  color: UniversalVariables.primaryCrimson,
+                  shape: BoxShape.rectangle,
+                  boxShadow: [
+                    BoxShadow(
+                        color: UniversalVariables.primaryEbony,
+                        offset: Offset(0.0, 1.0),
+                        blurRadius: 4.0)
+                  ]),
+              child: PreferredSize(
+                child: MainAppBar(back: "search", title: "Search"),
+                preferredSize: Size.fromHeight(media.height),
+              ),
+            ),
           ),
-          body: Container(
-              height: media.height,
-              width: media.width,
-              child: Stack(children: <Widget>[
-                Positioned(
-                    top: media.height * 0.45,
-                    left: media.width * 0.20,
-                    right: media.width * 0.20,
-                    child: SvgPicture.asset('assets/svgs/BarrenClipBoard.svg')),
-              ])))
-    ]);
+          Positioned(
+              top: media.height * 0.45,
+              left: media.width * 0.20,
+              right: media.width * 0.20,
+              child: SvgPicture.asset('assets/svgs/BarrenClipBoard.svg')),
+        ],
+      ),
+    );
   }
 }

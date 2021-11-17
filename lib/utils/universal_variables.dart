b045f1d6bd7705785dce054ca1d151cd6a3936ad
Taki Uddin <takiuddin93@gmail.com>
2021-11-17 16:09:28 +0600

    color variables added

--- lib/utils/universal_variables.dart
+++ lib/utils/universal_variables.dart
@@ -11,3 +11,15 @@ class UniversalVariables {
   static final Color primaryGhost = Color(0xFFCACFD6);
   static final Color primaryGhostShadow = Color(0x29CACFD6);
 }
+
+class PlusEqualsTo {
+  static final Color transparent = Color(0x00ffffff);
+  static final Color primaryColor = Color(0xffcd182c);
+  static final Color primaryLightColor = Color(0xffff5755);
+  static final Color primaryDarkColor = Color(0xff940004);
+  static final Color secondaryColor = Color(0xff2b2b2b);
+  static final Color secondaryLightColor = Color(0xff545454);
+  static final Color secondaryDarkColor = Color(0xff000000);
+  static final Color primaryTextColor = Color(0xfffafafa);
+  static final Color secondaryTextColor = Color(0xffffffff);
+}
